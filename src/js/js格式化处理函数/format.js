d7cee99843caad983cc41a7210266c80ae286557
YANJUNJIE <pcxiaoyan@gmail.com>
2021-12-06 15:33:15 +0800

    fix

---
+++ src/js/js格式化处理函数/format.js
@@ -0,0 +1,44 @@
+/**
+ * @param {*} val 传入的值 String | Number
+ * @param {*} config 配置
+ * @returns string
+ */
+function formatCurrency(val, config) {
+  const {
+    format = '$0,0.00',
+    prefix = '$', 
+    suffix = '', 
+    roundMethod  = null,
+    invalid = '-',
+    division = 1
+  } = config
+  if(Number.isNaN(Number(val)) || Array.isArray(val)) return invalid
+  val = Number(val) / division
+  const hasKilobit = format.indexOf(',') >= 0
+  const hasDecimal = format.indexOf('.') >= 0
+  const fmtDeliLen = hasDecimal ? format.split('.')[1].length : 0
+  const valStr = String(val)
+  let valDecimal = valStr.split('.')[1]
+  let integer = ''
+  let decimal = ''
+  if(hasDecimal) {
+    integer = valStr.split('.')[0]
+    decimal = valDecimal.length > fmtDeliLen
+      ? valDecimal.substr(0, fmtDeliLen) 
+      : valDecimal.padEnd(fmtDeliLen, '0')
+  } else {
+    integer =  roundMethod ? roundMethod.call(this, val) + '' : valStr.split('.')[0]
+  }
+  if(hasKilobit) {
+    integer = integer.replace(/(?!^)(?=(\d{3})+$)/g, ',')
+  }
+  return `${prefix}${integer}${hasDecimal ? '.' : ''}${decimal}${suffix}`
+}
+const res = formatCurrency('1321611312.991',
+  { format: '0,0',
+    suffix: 'M', 
+    roundMethod: Math.round,
+    division: 1000000 
+  }
+)
+console.log(res) // $1,322M
---
+++ src/js/js格式化处理函数/lodashDemo.js
@@ -0,0 +1,3 @@
+const _  = require('lodash')
+
+console.log(_.startsWith('aaabb', 'a'))
